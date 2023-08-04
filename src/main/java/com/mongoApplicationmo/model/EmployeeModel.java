a4b70b484ecc093ebbefc7cab0f196b9a7bb7b4a
kumarsushee <kumarsusheel549@gmail.com>
2023-08-04 22:04:05 +0530

    dfgd

--- src/main/java/com/mongoApplicationmo/model/EmployeeModel.java
+++ src/main/java/com/mongoApplicationmo/model/EmployeeModel.java
@@ -17,6 +17,12 @@ public class EmployeeModel {
 	
 	
 	
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
 	public String getName() {
 		return name;
 	}
